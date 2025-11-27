cb36bf9f2196137b276b09edb116c8268d876f1a
LKajamugan <l.kajamugan@gmail.com>
2025-11-27 11:14:07 +0530

    Create my_progress.py

---
+++ my_progress.py
@@ -0,0 +1,9 @@
+# You now have these skills SOLID:
+import pandas as pd                    # Data manipulation
+df = pd.read_csv()                     # Data loading
+df.shape                               # Data inspection  
+df.isnull().sum()                      # Data quality check
+df.groupby()[].mean()                  # Statistical analysis
+df[df['column'] < value]               # Data filtering
+df['column'].mean()                    # Summary statistics
+plt.hist() + plt.title() + plt.show()  # Data visualization
